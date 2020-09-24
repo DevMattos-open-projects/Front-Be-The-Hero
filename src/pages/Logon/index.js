7b6fa9774796ec4e8e9536cfa4bf566dbc000798
Rafael Matos <rafaelmatostj@gmail.com>
2020-09-24 18:01:17 -0300

    login-index

--- src/pages/Logon/index.js
+++ src/pages/Logon/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { FiLogIn } from 'react-icons/fi'
 
 import './style.css';
 
@@ -10,6 +11,18 @@ export default function Logon(){
        <div class="logon-container">
            <section class="form">
             <img src={logoImg} alt="Logo"/>
+              <form>
+                  <h1>Faça o seu Logon</h1>
+
+                  <input placeholder="Sua ID" />
+                  <button type="submit" >Entrar</button>
+
+                  <a href="/register">
+                      <FiLogIn size={16} color="#E02041"/>
+                      Não Tenho Cadastro
+                  </a>
+
+              </form>
            </section>
 
            <img src={heroesImg} alt="Heroes"/>
