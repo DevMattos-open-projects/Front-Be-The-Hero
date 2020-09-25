63cd1c6921033e9d1cd24dea17b59ad76ed905ec
Rafael Matos <rafaelmatostj@gmail.com>
2020-09-25 02:02:12 -0300

    profile

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,6 +1,22 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
+import { FiPower } from 'react-icons/fi';
+
+import './style.css';
+import logoImg from '../../assets/logo.svg'
 
 export default function Profile(){
-return <h1>Profile</h1>
+return (
+    <div className="profile-container">
+        <header>
+        <img src={logoImg} alt="Logo"/>
+        <span> Bem Vinda, APAD</span>
+        <Link className="button" to="/incidents/new" >Cadastrar novo caso</Link>
+        <button type="button">
+            <FiPower size={18} color="#E02041" />
+        </button>
+        </header>
+    </div>
+)
 
 }
