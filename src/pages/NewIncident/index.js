2b699badd8d4be4378755254274280d358036886
Rafael Matos <rafaelmatostj@gmail.com>
2020-10-06 22:12:02 -0300

    v - beta

--- src/pages/NewIncident/index.js
+++ src/pages/NewIncident/index.js
@@ -1,6 +1,8 @@
 import React, {useState} from 'react';
-import {Link} from 'react-router-dom';
+import {Link, useHistory } from 'react-router-dom';
 import {FiArrowLeft} from 'react-icons/fi'
+
+import api from '../../services/api';
 
 import logoImg from '../../assets/logo.svg'
 import './styles.css';
@@ -8,6 +10,34 @@ export default function NewIncident (){
   const [title, setTitle] = useState('');
   const [description, setDescription] = useState('');
   const [value, setValue] = useState('');
+
+  const history = useHistory();
+
+  const ongId = localStorage.getItem('ongId');
+
+  async function handleNewIncident(e){
+    e.preventDefault();
+
+    const data = {
+      title,
+      description,
+      value,
+    };
+
+    try {
+      await api.post('incidents', data, {
+        headers:{
+          Authorization: ongId,
+        }
+      })
+
+
+      history.push('/profile');
+
+    } catch(err) {
+      alert('Erro ao cadastrar.');
+    }
+  }
     return(  
     <div className="new-incident-container">
     <div className="content">
@@ -21,10 +51,22 @@ export default function NewIncident (){
           </Link>
       </section>
 
-      <form>
-      <input placeholder="Titulo do caso" />
-      <textarea placeholder="Descrição" />
-      <input placeholder="Valor em Reais" />
+      <form onSubmit={handleNewIncident}>
+      <input 
+       placeholder="Titulo do caso" 
+       value={title}
+       onChange={e =>setTitle(e.target.value)}
+       />
+      <textarea 
+       placeholder="Descrição" 
+       value={description}
+       onChange={e => setDescription(e.target.value)}
+       />
+      <input 
+       placeholder="Valor em Reais" 
+       value={value}
+       onChange={e => setValue(e.target.value)}
+       />
 
      
       <button className="button" type="submit"> Cadastrar</button>
